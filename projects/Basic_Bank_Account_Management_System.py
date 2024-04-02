4dd9e0717c1ac9f217fbe30d65a89ded1a6d9cb7
Yasemin Ibryam <yaseminibr123@gmail.com>
2024-04-02 13:28:09 +0300

    projects

--- projects/Basic_Bank_Account_Management_System.py
+++ projects/Basic_Bank_Account_Management_System.py
@@ -31,6 +31,7 @@ class SavingsAccount(BankAccount):
         """
         Constructor method to initialize the account number, balance, and interest rate.
         """
+        super().__init__(account_number, balance)
         self.account_number = account_number
         self.balance = balance
         self.interest_rate = interest_rate
@@ -39,24 +40,25 @@ class SavingsAccount(BankAccount):
         """
         Method to calculate and add interest to the account balance.
         """
-        # ✳️ Write code to calculate the interest based on the current balance and interest rate
-        # ✳️ Write code to add the calculated interest to the account balance
+        interest = self.balance * self.interest_rate/100
+        self.balance += interest
 
 
 # Testing the functionality of the classes
 if __name__ == "__main__":
+    bank_account = BankAccount(123456789, 1000)
 # ✳️ Create a BankAccount instance with account number "123456789" and initial balance of 1000
-
+    bank_account.deposit(500)
 # ✳️ Deposit 500 into the account
-
+    bank_account.withdraw(200)
 # ✳️ Withdraw 200 from the account
-
+    print(bank_account.get_balance())
 # ✳️ Get the current balance of the bank account
-
+    savings_account = SavingsAccount(987654321, 2000, 5)
 # ✳️ Create a SavingsAccount instance with account number "987654321", initial balance of 2000, and interest rate of 5%
-
+    savings_account.deposit(1000)
 # ✳️ Deposit 1000 into the savings account
-
+    savings_account.calculate_interest()
 # ✳️ Calculate and add interest to the savings account
-
+    print(savings_account.get_balance())
 # ✳️ Get the current balance of the savings account after adding interest
